8ce098c4f87140b0a00229261571e087bb305297
= <=>
2019-05-25 01:47:22 +0530

    pair of nodes with given sum in BST

--- src/com/java/trees/InorderIterative.java
+++ src/com/java/trees/InorderIterative.java
@@ -4,40 +4,74 @@ import com.java.trees.BinaryTree.Node;
 import java.util.*;
 
 public class InorderIterative {
-	
+
 	static Node root;
 	static Stack<Node>stack=new Stack<>();
+	static Stack<Node>revstack=new Stack<>();
 	static ArrayList<Integer> list=new ArrayList<>();
-	
-	public static ArrayList<Integer> findInorder(Node root)
+	static int elem1=0;
+	static int elem2=0;
+
+	public static void sumInorder(Node root,Node root2,boolean inflag,boolean revinflag,int k)
 	{
-		if(root==null)
+		if(root==null||root2==null)
 		{
-			return null;
+			return;
 		}
-		
-		
-		//findInorder(root.left);
-		while(root!=null)
+
+		while(root!=null || stack.size()>0||revstack.size()>0||root2!=null)
 		{
-			stack.push(root);
-			root=root.left;
-		}
-		
-		while(!stack.isEmpty())
-		{
-			Node node=stack.pop();
-			list.add(node.data);
-			node=node.right;
-			
-			while(node!=null)
+			if(inflag==true)
+			{
+				while(root!=null)
 				{
-				stack.push(node);
-				node=node.left;
+					stack.push(root);
+					root=root.left;
 				}
-		}
-		return list;
+
+				root=stack.pop();
+				elem1=root.data;
+				//list.add(root.data);
+				root=root.right;
+
+			}
+			if(revinflag==true)
+			{
+				while(root2!=null)
+				{
+					revstack.push(root2);
+					root2=root2.right;
+				}
+
+				root2=revstack.pop();
+				elem2=root2.data;
+				//list.add(root.data);
+				root2=root2.left;
+
+			}
 		
+			if(elem1+elem2==k)
+			{
+				System.out.println("Pairs are" +"\t"+ elem1+"\t"+elem2);
+				return;
+			}
+			if(elem1+elem2<k)
+			{
+				inflag=true;
+				revinflag=false;
+
+			}
+			if(elem1+elem2>k)
+			{
+				revinflag=true;
+				inflag=false;
+
+			}
+
+
+		}
+		
+
 	}
 
 	public static void main(String[] args)
@@ -51,13 +85,13 @@ public class InorderIterative {
 		root.left.right.left=new Node(4);
 		root.right.right=new Node(14);
 		root.right.right.left=new Node(13);
-		
-		ArrayList<Integer> list=findInorder(root);
-		
-		for (Integer e : list) 
-		{
-			System.out.println(e);
-		}
+
+		sumInorder(root,root,true,true,7);
+
+		//		for (Integer e : list) 
+		//		{
+		//			System.out.println(e);
+		//		}
 
 	}
 
