cbf3d73b9d183054cf23a09e60e65a872c1b3e55
sanchitarath <sanchitarath@192.168.1.3>
2019-09-10 20:05:02 +0530

    Longest increasing subsequence along with length and sequence.

---
+++ src/com/java/dynamicprog/LIS.java
@@ -0,0 +1,45 @@
+package com.java.dynamicprog;
+
+import java.util.Arrays;
+
+public class LIS {
+
+	public static int findLengthLIS(int[]arr)
+	{
+		int n=arr.length;
+		int maxlength=0;
+		int result=1;
+		int[] temp=new int[n];
+		Arrays.fill(temp, 1);
+		
+		for (int i = 1; i < n; i++) 
+		{
+			for(int j=0;j<i;j++)
+			{
+				if(arr[j]<arr[i]&& (1+temp[j])>result)
+				{
+					temp[i]=1+temp[j];
+					result=temp[i];
+				}
+			}
+		}
+		
+		for (int i = 0; i < n; i++) 
+		{
+			if(temp[i]>maxlength)
+			{
+				maxlength=temp[i];
+				System.out.println(arr[i]);
+			}
+		}
+	return maxlength;
+	}
+	public static void main(String[] args) 
+	{
+		int[] arr={10,22,9,33,21,50,41,60,80};
+		int length=findLengthLIS(arr);
+		System.out.println("Length of longest increasing subsequence is "+ length);
+
+	}
+
+}
